05b568390ffea7af25e027a8e30bec414c383f36
sulehaamin <149660535+sulehaamin@users.noreply.github.com>
2023-11-02 03:38:03 -0400

    Create 90x728.js

---
+++ 90x728.js
@@ -0,0 +1,8 @@
+	atOptions = {
+		'key' : '6bd55294e407ccbc9175ec293174f8b0',
+		'format' : 'iframe',
+		'height' : 90,
+		'width' : 728,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highcpmcreativeformat.com/6bd55294e407ccbc9175ec293174f8b0/invoke.js"></scr' + 'ipt>');
