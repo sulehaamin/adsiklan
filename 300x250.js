7ce6a76820a719895a1b569a0733c794024f0a84
sulehaamin <149660535+sulehaamin@users.noreply.github.com>
2023-11-09 18:32:43 +0800

    Create 300x250.js

---
+++ 300x250.js
@@ -0,0 +1,8 @@
+atOptions = {
+		'key' : '112f63885b93108b2b1ac2ac02a5f9f3',
+		'format' : 'iframe',
+		'height' : 250,
+		'width' : 300,
+		'params' : {}
+	};
+	document.write('<scr' + 'ipt type="text/javascript" src="//www.highcpmcreativeformat.com/112f63885b93108b2b1ac2ac02a5f9f3/invoke.js"></scr' + 'ipt>');
